512d32369170f2a899b5e935e879a7555ffb53f9
Your Name <youremail@example.com>
2026-05-23 22:16:41 +0300

    fix: remove dotenv from passport.js

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -1,13 +1,12 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import User from '../models/User.js';
-import dotenv from 'dotenv';
-dotenv.config();
+
 
 passport.use(new GoogleStrategy({
   clientID:     process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL:  'http://localhost:5000/api/auth/google/callback',
+callbackURL: process.env.GOOGLE_CALLBACK_URL
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ email: profile.emails[0].value });
